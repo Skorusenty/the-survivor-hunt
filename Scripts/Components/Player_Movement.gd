deee541e8d8012c0ca8b1d2d2ad8e1eb654c1464
Skorusenty <kacper.skorus05@gmail.com>
2025-11-30 12:37:01 +0100

    modulated movement

--- Scripts/Components/Player_Movement.gd
+++ Scripts/Components/Player_Movement.gd
@@ -1,17 +1,77 @@
 extends Node
 
 #Player ref.
-@onready var player: CharacterBody3D = $".."
+@onready var parent: CharacterBody3D = $".."
 #Velocity
+var delta: float
 var velocity: Vector3 = Vector3.ZERO
+@export var base_acceleration: float = 10.0
+var acceleration: float
 #Inputs
-var move_input: Vector2 = Vector2.ZERO
+var direction: Vector3
 #Speeds
-@export var walk_speed : float = 10
-@export var sprint_speed : float = 20
-@export var crouch_speed : float = 5
+var speed
+@export var walk_speed : float = 10.0
+@export var sprint_speed : float = 20.0
+@export var crouch_speed : float = 5.0
+@export var jump_power : float = 10.0
 #Gravity
 var gravity : Variant = ProjectSettings.get_setting("physics/3d/default_gravity")
 #Booleans
+var ground_check: bool = false
 var is_crouching: bool = false
 var is_sprinting: bool = false
+var jump_pressed: bool = false
+
+func _ready() -> void:
+	speed = walk_speed
+
+func set_direction(value: Vector3):
+	direction = value
+
+func set_is_on_floor(value: bool):
+	ground_check = value
+
+func set_jump_pressed(value: bool):
+	jump_pressed = value
+
+func set_crouch(value: bool):
+	is_crouching = value
+
+func set_sprint(value: bool):
+	is_sprinting = value
+
+func set_delta(value: float):
+	delta = value
+
+func process_movement():
+	#State check
+	if is_crouching:
+		speed = crouch_speed
+		acceleration = base_acceleration * 0.5
+	elif is_sprinting:
+		speed = sprint_speed
+		acceleration = base_acceleration * 1.5
+	else:
+		speed = walk_speed
+		acceleration = base_acceleration
+
+	#Jump
+	if jump_pressed and ground_check:
+		velocity.y = jump_power
+	#Gravity
+	if not ground_check:
+		velocity.y -= gravity * delta
+	#Movement depending on direction
+	if direction:
+		velocity.x = move_toward(velocity.x, direction.x * speed, acceleration * delta)
+		velocity.z = move_toward(velocity.z, direction.z * speed, acceleration * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, acceleration * delta)
+		velocity.z = move_toward(velocity.z, 0, acceleration * delta)
+	
+
+
+func get_velocity():
+	return velocity
+	
--- Scripts/Systems/Player_Manager.gd
+++ Scripts/Systems/Player_Manager.gd
@@ -6,15 +6,8 @@ extends CharacterBody3D
 @export var collision: CollisionShape3D
 @export var movement_component: Node3D
 #MOVEMENT VALUES
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var speed
-const default_move_speed = 3
-const sprint_move_speed = 5
-const jump_speed = 3
-const crouch_move_speed = 1
-const crouch_speed = 20
+#@export var toggle_crouch_button: bool = true
 var is_crouching: bool = false
-@export var toggle_crouch_button: bool = true
 #BOB VALUES
 const bob_freq = 2.0
 const bob_amp = 0.08
@@ -29,15 +22,12 @@ const fov_change = 1.5
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
-
 func _input(event):
 	if event.is_action_pressed("Exit"):
 		get_tree().quit()
-	if event.is_action_pressed("Crouch"):
-		toggle_crouch()
+	movement_component.set_jump_pressed(Input.is_action_just_pressed("Jump"))
 
 func _unhandled_input(event):
-	
 	#CAMERA ROTATION WITH HEAD/WHOLE CHARACTER MOVEMENT
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * sensitivity)
@@ -45,63 +35,34 @@ func _unhandled_input(event):
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
 func _ready():
-
 	#GET MOUSE POS
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	speed = default_move_speed
 
 func _physics_process(delta):
-	#GRAVITY
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	#JUMP
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = jump_speed
-
-	#MOVEMENT
 	var input_vector = -Input.get_vector("Left", "Right", "Forward", "Backward")
-	var direction = (head.transform.basis * Vector3(input_vector.x, 0, input_vector.y)).normalized()
-	movement_component.set_input(input_vector)
+	var world_direction = (head.transform.basis * Vector3(input_vector.x, 0, input_vector.y)).normalized()
+	movement_component.set_direction(world_direction)
+	movement_component.set_is_on_floor(is_on_floor())
+	movement_component.set_crouch(Input.is_action_pressed("Crouch"))
+	movement_component.set_sprint(Input.is_action_pressed("Sprint"))
+	movement_component.set_delta(delta)
+	movement_component.process_movement()
 	velocity = movement_component.get_velocity()
-	#if direction:
-		#velocity.x = direction.x * speed
-		#velocity.z = direction.z * speed
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, default_move_speed)
-		#velocity.z = move_toward(velocity.z, 0, default_move_speed)
-
 	#BOB VALUES
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
-
-	#SPRINT
-	if Input.is_action_pressed("Sprint") and is_crouching == false:
-		movement_speed("Sprint")
-
 	#FOV CHANGE
-	var velocity_clamped = clamp(velocity.length(), 0.5, sprint_move_speed * 2)
-	var target_fov = base_fov + fov_change * velocity_clamped
-	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-
+	#var velocity_clamped = clamp(velocity.length(), 0.5, sprint_move_speed * 2)
+	#var target_fov = base_fov + fov_change * velocity_clamped
+	#camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 	move_and_slide()
 
 func _headbob(time) -> Vector3:
-
 	#HEAD MOVEMENT
 	var pos = Vector3.ZERO
 	pos.y = sin(time * bob_freq) * bob_amp
 	pos.x = cos(time * bob_freq / 2) * bob_amp
 	return pos
-
-func toggle_crouch():
-
-	#CROUCHING
-		if is_crouching == true:
-			crouch(false)
-		elif is_crouching == false:
-			crouch(true)
-		is_crouching = !is_crouching
 
 func crouch(state: bool):
 	match state:
@@ -109,13 +70,3 @@ func crouch(state: bool):
 			animplayer.play("Crouch")
 		false:
 			animplayer.play_backwards("Crouch")
-
-
-func movement_speed(state: String):
-	match state:
-		"Crouch":
-			speed = crouch_move_speed
-		"Default":
-			speed = default_move_speed
-		"Sprint":
-			speed = sprint_move_speed
